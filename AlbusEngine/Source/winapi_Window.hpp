313161afbca751a486731704a3d64ea454a2d67d
Shirotsu <albus0sh@gmail.com>
2019-04-01 05:06:49 +0900

    プログラムのインデントをタブから複数の空白へ変更

--- AlbusEngine/Source/winapi_Window.hpp
+++ AlbusEngine/Source/winapi_Window.hpp
@@ -6,22 +6,22 @@ namespace shi62::winapi{
 // 画面に表示するウィンドウのクラス
 class Window{
 public:
-	Window( const char* windowClassName, const char* windowTitle, const int width, const int height );
-	~Window();
-	
-	// ループ処理一回分を実行
-	void Update();
+    Window( const char* windowClassName, const char* windowTitle, const int width, const int height );
+    ~Window();
 
-	// ウィンドウをterminateするか否か
-	bool TerminationRequested() const;
+    // ループ処理一回分を実行
+    void Update();
+
+    // ウィンドウをterminateするか否か
+    bool TerminationRequested() const;
 
 private:
-	HINSTANCE mInstanceHandle;
-	LPCSTR mWindowClassName;
-	LPCSTR mWindowTitle; // Windowタイトルバーに表示する文字列
-	HWND mWindowHandle;
-	MSG mMessage;
-	BOOL mMessageState; // メッセージ受信時に終了/エラーを検出する変数
+    HINSTANCE mInstanceHandle;
+    LPCSTR mWindowClassName;
+    LPCSTR mWindowTitle; // Windowタイトルバーに表示する文字列
+    HWND mWindowHandle;
+    MSG mMessage;
+    BOOL mMessageState; // メッセージ受信時に終了/エラーを検出する変数
 };
 
 // キーボードのいずれかのキー押下時のメッセージ処理関数
