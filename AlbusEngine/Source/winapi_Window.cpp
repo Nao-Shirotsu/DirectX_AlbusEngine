67deec2a72db187280ccc92558e7a9328968ee58
Shirotsu <albus0sh@gmail.com>
2019-04-30 19:17:23 +0900

    コンストラクタの処理をprivate関数に分割

--- AlbusEngine/Source/winapi_Window.cpp
+++ AlbusEngine/Source/winapi_Window.cpp
@@ -1,7 +1,7 @@
 ﻿#include <crtdbg.h>
 
-#include "winapi_Window.hpp"
 #include "d3d11_Core.hpp"
+#include "winapi_Window.hpp"
 
 namespace shi62::winapi {
 
@@ -11,98 +11,98 @@ Window::Window(HINSTANCE instanceHandle, LPCWSTR windowClassName, LPCWSTR window
     , mWindowTitle(windowTitle)
     , mMessage({ 0 })
     , mMessageState(TRUE) {
-    WNDCLASS windowClass;
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    windowClass.lpfnWndProc = [](HWND windowHandle,
-                                 UINT message,
-                                 WPARAM wParam,
-                                 LPARAM lParam) -> LRESULT CALLBACK {
-        switch (message) {
-        case WM_PAINT:
-            PAINTSTRUCT paintStruct;
-            HDC hdc;
-            hdc = BeginPaint(windowHandle, &paintStruct);
-            EndPaint(windowHandle, &paintStruct);
-            break;
+  WNDCLASS windowClass;
+  windowClass.style = CS_HREDRAW | CS_VREDRAW;
+  windowClass.lpfnWndProc = [](HWND windowHandle,
+                               UINT message,
+                               WPARAM wParam,
+                               LPARAM lParam) -> LRESULT CALLBACK {
+    switch (message) {
+    case WM_PAINT:
+      PAINTSTRUCT paintStruct;
+      HDC hdc;
+      hdc = BeginPaint(windowHandle, &paintStruct);
+      EndPaint(windowHandle, &paintStruct);
+      break;
 
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
 
-        case WM_KEYDOWN:
-            ProcessKeydownMessage(windowHandle, wParam);
-            break;
+    case WM_KEYDOWN:
+      ProcessKeydownMessage(windowHandle, wParam);
+      break;
 
-        default:
-            break;
-        }
+    default:
+      break;
+    }
 
-        return DefWindowProc(windowHandle, message, wParam, lParam);
-    };
-    windowClass.cbClsExtra = 0;
-    windowClass.cbWndExtra = 0;
-    windowClass.hInstance = mInstanceHandle;
-    windowClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-    windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    windowClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
-    windowClass.lpszMenuName = nullptr;
-    windowClass.lpszClassName = windowClassName;
+    return DefWindowProc(windowHandle, message, wParam, lParam);
+  };
+  windowClass.cbClsExtra = 0;
+  windowClass.cbWndExtra = 0;
+  windowClass.hInstance = mInstanceHandle;
+  windowClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+  windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  windowClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+  windowClass.lpszMenuName = nullptr;
+  windowClass.lpszClassName = windowClassName;
 
-    auto res = RegisterClass(&windowClass);
+  auto res = RegisterClass(&windowClass);
 
-    // ウィンドウクラス登録失敗でASSERT
-    _ASSERT_EXPR(res, TEXT("Class Registration Error"));
+  // ウィンドウクラス登録失敗でASSERT
+  _ASSERT_EXPR(res, TEXT("Class Registration Error"));
 
-    RECT windowRect = { 0, 0, width, height };
-    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, TRUE);
+  RECT windowRect = { 0, 0, width, height };
+  AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, TRUE);
 
-    mWindowHandle = CreateWindow(mWindowClassName,
-                                 mWindowTitle,
-                                 WS_OVERLAPPEDWINDOW,
-                                 CW_USEDEFAULT,
-                                 CW_USEDEFAULT,
-                                 windowRect.right - windowRect.left,
-                                 windowRect.bottom - windowRect.top,
-                                 nullptr,
-                                 nullptr,
-                                 mInstanceHandle,
-                                 nullptr);
+  mWindowHandle = CreateWindow(mWindowClassName,
+                               mWindowTitle,
+                               WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT,
+                               CW_USEDEFAULT,
+                               windowRect.right - windowRect.left,
+                               windowRect.bottom - windowRect.top,
+                               nullptr,
+                               nullptr,
+                               mInstanceHandle,
+                               nullptr);
 
-    // ウィンドウ生成失敗でASSERT
-    _ASSERT_EXPR(mWindowHandle, TEXT("Window Creation Error"));
+  // ウィンドウ生成失敗でASSERT
+  _ASSERT_EXPR(mWindowHandle, TEXT("Window Creation Error"));
 
-    ShowWindow(mWindowHandle, SW_SHOWNORMAL);
-    UpdateWindow(mWindowHandle);
+  ShowWindow(mWindowHandle, SW_SHOWNORMAL);
+  UpdateWindow(mWindowHandle);
 }
 
 Window::~Window() {
-    UnregisterClass(mWindowClassName, mInstanceHandle);
-    mWindowHandle = nullptr;
+  UnregisterClass(mWindowClassName, mInstanceHandle);
+  mWindowHandle = nullptr;
 }
 
 auto Window::Update() -> void {
-    mMessageState = PeekMessage(&mMessage, nullptr, 0, 0, PM_REMOVE);
-    if (!mMessageState) {
-        return;
-    }
-    TranslateMessage(&mMessage);
-    DispatchMessage(&mMessage);
+  mMessageState = PeekMessage(&mMessage, nullptr, 0, 0, PM_REMOVE);
+  if (!mMessageState) {
+    return;
+  }
+  TranslateMessage(&mMessage);
+  DispatchMessage(&mMessage);
 }
 
 auto Window::TerminationRequested() -> bool const {
-    return mMessage.message == WM_QUIT || mMessageState == -1; // 終了時とエラー発生時
+  return mMessage.message == WM_QUIT || mMessageState == -1; // 終了時とエラー発生時
 }
 
 auto ProcessKeydownMessage(HWND windowHandle, WPARAM wParam) -> void {
-    switch (wParam) {
-    case VK_ESCAPE:
-        PostMessage(windowHandle, WM_CLOSE, 0, 0);
-        break;
-    }
+  switch (wParam) {
+  case VK_ESCAPE:
+    PostMessage(windowHandle, WM_CLOSE, 0, 0);
+    break;
+  }
 }
 
 }
 
 auto shi62::winapi::Window::GetWindowHandle() -> HWND const {
-    return mWindowHandle;
+  return mWindowHandle;
 }
--- AlbusEngine/Source/winapi_Window.hpp
+++ AlbusEngine/Source/winapi_Window.hpp
@@ -7,28 +7,26 @@ namespace shi62::winapi {
 // 画面に表示するウィンドウのクラス
 class Window {
 public:
-    Window(HINSTANCE instanceHandle, LPCWSTR windowClassName, LPCWSTR windowTitle, const int width, const int height);
+  Window(HINSTANCE instanceHandle, LPCWSTR windowClassName, LPCWSTR windowTitle, const int width, const int height);
 
-    ~Window();
+  ~Window();
 
-    // ループ処理一回分を実行
-    auto Update() -> void;
+  // ループ処理一回分を実行
+  auto Update() -> void;
 
-    // ウィンドウ終了処理が始まるときtrue
-    [[nodiscard]]
-    auto TerminationRequested() -> bool const;
+  // ウィンドウ終了処理が始まるときtrue
+  [[nodiscard]] auto TerminationRequested() -> bool const;
 
-    // mWindowHandleをぶん投げる
-    [[nodiscard]]
-    auto GetWindowHandle() -> HWND const;
+  // mWindowHandleをぶん投げる
+  [[nodiscard]] auto GetWindowHandle() -> HWND const;
 
 private:
-    HINSTANCE mInstanceHandle;
-    LPCWSTR mWindowClassName;
-    LPCWSTR mWindowTitle; // Windowタイトルバーに表示する文字列
-    HWND mWindowHandle;
-    MSG mMessage;
-    BOOL mMessageState; // メッセージ受信時に終了/エラーを検出する変数
+  HINSTANCE mInstanceHandle;
+  LPCWSTR mWindowClassName;
+  LPCWSTR mWindowTitle; // Windowタイトルバーに表示する文字列
+  HWND mWindowHandle;
+  MSG mMessage;
+  BOOL mMessageState; // メッセージ受信時に終了/エラーを検出する変数
 };
 
 // キーボードのいずれかのキー押下時のメッセージ処理関数
