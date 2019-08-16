b716a7ba44d813bfe88c67b80611920afb7b6d77
Shirotsu <albus0sh@gmail.com>
2019-08-17 07:00:48 +0900

    リファクタ: Windowクラスを分離

--- AlbusEngine12/src/WinMain.cpp
+++ AlbusEngine12/src/WinMain.cpp
@@ -1,19 +1,4 @@
-﻿// AlbusEngine12.cpp : アプリケーションのエントリ ポイントを定義します。
-//
-
-#include <windows.h>
-
-#define WIN32_LEAN_AND_MEAN
-
-// グローバル変数:
-HINSTANCE hInst; // 現在のインターフェイス
-
-// このコード モジュールに含まれる関数の宣言を転送します:
-ATOM MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName);
-BOOL InitInstance(HINSTANCE, int, LPCWSTR, LPCWSTR);
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
-void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam);
+﻿#include "winapi_Window.hpp"
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_opt_ HINSTANCE hPrevInstance,
@@ -21,139 +6,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_ int nCmdShow) {
   UNREFERENCED_PARAMETER(hPrevInstance);
   UNREFERENCED_PARAMETER(lpCmdLine);
+  UNREFERENCED_PARAMETER(nCmdShow);
 
-  LPCWSTR winClass = L"dx12Handler";
-  LPCWSTR winTitle = L"DirectX12 Test";
-
-  MyRegisterClass(hInstance, winClass);
-
-  // アプリケーション初期化の実行:
-  if (!InitInstance(hInstance, nCmdShow, winClass, winTitle)) {
-    return FALSE;
+  shi62::winapi::Window windowInstance{ hInstance, L"dx12 window handler", L"DirectX12 Test", 800, 600 };
+  while (!windowInstance.TerminationRequested()) {
+    windowInstance.Update();
   }
 
-  MSG msg;
-
-  // メイン メッセージ ループ:
-  while (GetMessage(&msg, nullptr, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
-
-  return ( int )msg.wParam;
-}
-
-
-//
-//  関数: MyRegisterClass()
-//
-//  目的: ウィンドウ クラスを登録します。
-//
-ATOM MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName) {
-  WNDCLASSEXW wcex;
-  wcex.cbSize = sizeof(WNDCLASSEX);
-  wcex.style = CS_HREDRAW | CS_VREDRAW;
-  wcex.lpfnWndProc = WndProc;
-  wcex.cbClsExtra = 0;
-  wcex.cbWndExtra = 0;
-  wcex.hInstance = hInstance;
-  wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-  wcex.lpszMenuName = nullptr;
-  wcex.lpszClassName = windowClassName;
-  wcex.hIconSm = nullptr;
-
-  return RegisterClassExW(&wcex);
-}
-
-//
-//   関数: InitInstance(HINSTANCE, int)
-//
-//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します
-//
-//   コメント:
-//
-//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
-//        メイン プログラム ウィンドウを作成および表示します。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, LPCWSTR windowClassName, LPCWSTR windowTitle) {
-  hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
-
-  HWND hWnd = CreateWindowW(windowClassName, windowTitle, WS_OVERLAPPEDWINDOW,
-                            CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-  if (!hWnd) {
-    return FALSE;
-  }
-
-  ShowWindow(hWnd, nCmdShow);
-  UpdateWindow(hWnd);
-
-  return TRUE;
-}
-
-//
-//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的: メイン ウィンドウのメッセージを処理します。
-//
-//  WM_COMMAND  - アプリケーション メニューの処理
-//  WM_PAINT    - メイン ウィンドウを描画する
-//  WM_DESTROY  - 中止メッセージを表示して戻る
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  switch (message) {
-    case WM_PAINT: {
-      PAINTSTRUCT ps;
-      HDC hdc = BeginPaint(hWnd, &ps);
-      // TODO: HDC を使用する描画コードをここに追加してください...
-      EndPaint(hWnd, &ps);
-      break;
-    }
-
-    case WM_DESTROY: {
-      PostQuitMessage(0);
-      break;
-    }
-
-    case WM_KEYDOWN: {
-      ProcessKeydownMessage(hWnd, wParam);
-      break;
-    }
-
-    default: {
-      break;
-    }
-  }
-  return DefWindowProc(hWnd, message, wParam, lParam);
-}
-
-// バージョン情報ボックスのメッセージ ハンドラーです。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
-  UNREFERENCED_PARAMETER(lParam);
-  switch (message) {
-    case WM_INITDIALOG: {
-      return static_cast<INT_PTR>(TRUE);
-    }
-
-    case WM_COMMAND: {
-      if (LOWORD(wParam) != IDOK && LOWORD(wParam) != IDCANCEL) {
-        break;
-      }
-      EndDialog(hDlg, LOWORD(wParam));
-      return static_cast<INT_PTR>(TRUE);
-    }
-  }
-  return static_cast<INT_PTR>(FALSE);
-}
-
-void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam) {
-  switch (wParam) {
-    case VK_ESCAPE: {
-      PostMessage(windowHandle, WM_CLOSE, 0, 0);
-      break;
-    }
-  }
+  return 0;
 }
---
+++ AlbusEngine12/src/winapi_Window.cpp
@@ -0,0 +1,118 @@
+﻿#include <crtdbg.h>
+
+#include "winapi_Window.hpp"
+
+namespace shi62::winapi {
+
+// ウィンドウプロシージャ
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+// キーボードのいずれかのキー押下時のメッセージ処理関数
+void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam);
+
+Window::Window(HINSTANCE instanceHandle, LPCWSTR windowClassName, LPCWSTR windowTitle, const int width, const int height)
+    : mInstanceHandle(instanceHandle)
+    , mWindowClassName(windowClassName)
+    , mWindowTitle(windowTitle)
+    , mMessage({ 0 })
+    , mMessageState(TRUE) {
+  WNDCLASSEXW windowClass;
+  windowClass.cbSize = sizeof(WNDCLASSEXW);
+  windowClass.style = CS_HREDRAW | CS_VREDRAW;
+  windowClass.lpfnWndProc = WindowProcedure;
+  windowClass.cbClsExtra = 0;
+  windowClass.cbWndExtra = 0;
+  windowClass.hInstance = mInstanceHandle;
+  windowClass.hIcon = LoadIcon(mInstanceHandle, IDI_APPLICATION);
+  windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  windowClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+  windowClass.lpszMenuName = nullptr;
+  windowClass.lpszClassName = windowClassName;
+  windowClass.hIconSm = nullptr;
+
+  ATOM res = RegisterClassExW(&windowClass);
+
+  // ウィンドウクラス登録失敗でASSERT
+  _ASSERT_EXPR(res, TEXT("Class Registration Error"));
+
+  RECT windowRect = { 0, 0, width, height };
+  AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, TRUE);
+
+  mWindowHandle = CreateWindowW(mWindowClassName,
+                                mWindowTitle,
+                                WS_OVERLAPPEDWINDOW,
+                                CW_USEDEFAULT,
+                                CW_USEDEFAULT,
+                                windowRect.right - windowRect.left,
+                                windowRect.bottom - windowRect.top,
+                                nullptr,
+                                nullptr,
+                                mInstanceHandle,
+                                nullptr);
+
+  // ウィンドウ生成失敗でASSERT
+  _ASSERT_EXPR(mWindowHandle, TEXT("Window Creation Error"));
+
+  ShowWindow(mWindowHandle, SW_SHOWNORMAL);
+  UpdateWindow(mWindowHandle);
+}
+
+Window::~Window() {
+  UnregisterClass(mWindowClassName, mInstanceHandle);
+  mWindowHandle = nullptr;
+}
+
+void Window::Update() {
+  mMessageState = PeekMessage(&mMessage, nullptr, 0, 0, PM_REMOVE);
+  if (!mMessageState) {
+    return;
+  }
+  TranslateMessage(&mMessage);
+  DispatchMessage(&mMessage);
+}
+
+bool Window::TerminationRequested() const {
+  return mMessage.message == WM_QUIT || mMessageState == -1; // 終了時とエラー発生時
+}
+
+HWND Window::GetWindowHandle() const {
+  return mWindowHandle;
+}
+
+void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam) {
+  switch (wParam) {
+    case VK_ESCAPE: {
+      PostMessage(windowHandle, WM_CLOSE, 0, 0);
+      break;
+    }
+  }
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+  switch (message) {
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hWnd, &ps);
+      // TODO: HDC を使用する描画コードをここに追加してください...
+      EndPaint(hWnd, &ps);
+      break;
+    }
+
+    case WM_DESTROY: {
+      PostQuitMessage(0);
+      break;
+    }
+
+    case WM_KEYDOWN: {
+      ProcessKeydownMessage(hWnd, wParam);
+      break;
+    }
+
+    default: {
+      break;
+    }
+  }
+  return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+} // namespace shi62::winapi
