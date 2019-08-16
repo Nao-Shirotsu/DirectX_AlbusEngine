e027373a3acffe837835897530d8ba4451f9ae74
Shirotsu <albus0sh@gmail.com>
2019-08-17 06:19:35 +0900

    ウィンドウ表示までOK(bug fixed)

--- AlbusEngine12/AlbusEngine12.cpp
+++ AlbusEngine12/AlbusEngine12.cpp
@@ -1,50 +1,47 @@
 ﻿// AlbusEngine12.cpp : アプリケーションのエントリ ポイントを定義します。
 //
 
-#include "framework.h"
-//#include "AlbusEngine12.h"
+#include <windows.h>
 
-#define MAX_LOADSTRING 100
+#define WIN32_LEAN_AND_MEAN
 
 // グローバル変数:
-HINSTANCE hInst;                                // 現在のインターフェイス
+HINSTANCE hInst; // 現在のインターフェイス
 
 // このコード モジュールに含まれる関数の宣言を転送します:
-ATOM                MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName);
-BOOL                InitInstance(HINSTANCE, int, LPCWSTR, LPCWSTR);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-void				ProcessKeydownMessage(HWND windowHandle, WPARAM wParam);
+ATOM MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName);
+BOOL InitInstance(HINSTANCE, int, LPCWSTR, LPCWSTR);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow) {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+                      _In_opt_ HINSTANCE hPrevInstance,
+                      _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
 
-	LPCWSTR winClass = L"dx12Handler";
-	LPCWSTR winTitle = L"DirectX12 Test";
+  LPCWSTR winClass = L"dx12Handler";
+  LPCWSTR winTitle = L"DirectX12 Test";
 
-	MyRegisterClass(hInstance, winClass);
+  MyRegisterClass(hInstance, winClass);
 
-	// アプリケーション初期化の実行:
-	if (!InitInstance(hInstance, nCmdShow, winClass, winTitle)) {
-		return FALSE;
-	}
+  // アプリケーション初期化の実行:
+  if (!InitInstance(hInstance, nCmdShow, winClass, winTitle)) {
+    return FALSE;
+  }
 
-	MSG msg;
+  MSG msg;
 
-	// メイン メッセージ ループ:
-	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+  // メイン メッセージ ループ:
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
 
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return (int)msg.wParam;
+  return ( int )msg.wParam;
 }
-
 
 
 //
@@ -53,21 +50,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //  目的: ウィンドウ クラスを登録します。
 //
 ATOM MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName) {
-	WNDCLASSEXW wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = nullptr;
-	wcex.lpszClassName = windowClassName;
-	wcex.hIconSm = nullptr;
+  WNDCLASSEXW wcex;
+  wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = nullptr;
+  wcex.lpszClassName = windowClassName;
+  wcex.hIconSm = nullptr;
 
-	return RegisterClassExW(&wcex);
+  return RegisterClassExW(&wcex);
 }
 
 //
@@ -81,19 +78,19 @@ ATOM MyRegisterClass(HINSTANCE hInstance, LPCWSTR windowClassName) {
 //        メイン プログラム ウィンドウを作成および表示します。
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, LPCWSTR windowClassName, LPCWSTR windowTitle) {
-	hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
+  hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
 
-	HWND hWnd = CreateWindowW(windowClassName, windowTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+  HWND hWnd = CreateWindowW(windowClassName, windowTitle, WS_OVERLAPPEDWINDOW,
+                            CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-	if (!hWnd) {
-		return FALSE;
-	}
+  if (!hWnd) {
+    return FALSE;
+  }
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
-	return TRUE;
+  return TRUE;
 }
 
 //
@@ -107,49 +104,56 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, LPCWSTR windowClassName, LP
 //
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	switch (message) {
-	case WM_PAINT:
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		// TODO: HDC を使用する描画コードをここに追加してください...
-		EndPaint(hWnd, &ps);
-		break;
+  switch (message) {
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hWnd, &ps);
+      // TODO: HDC を使用する描画コードをここに追加してください...
+      EndPaint(hWnd, &ps);
+      break;
+    }
 
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
+    case WM_DESTROY: {
+      PostQuitMessage(0);
+      break;
+    }
 
-	case WM_KEYDOWN:
-		ProcessKeydownMessage(hWnd, wParam);
-		break;
+    case WM_KEYDOWN: {
+      ProcessKeydownMessage(hWnd, wParam);
+      break;
+    }
 
-	default:
-		break;
-	}
-	return DefWindowProc(hWnd, message, wParam, lParam);
+    default: {
+      break;
+    }
+  }
+  return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
 // バージョン情報ボックスのメッセージ ハンドラーです。
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message) {
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message) {
+    case WM_INITDIALOG: {
+      return static_cast<INT_PTR>(TRUE);
+    }
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
+    case WM_COMMAND: {
+      if (LOWORD(wParam) != IDOK && LOWORD(wParam) != IDCANCEL) {
+        break;
+      }
+      EndDialog(hDlg, LOWORD(wParam));
+      return static_cast<INT_PTR>(TRUE);
+    }
+  }
+  return static_cast<INT_PTR>(FALSE);
 }
 
 void ProcessKeydownMessage(HWND windowHandle, WPARAM wParam) {
-	switch (wParam) {
-	case VK_ESCAPE:
-		PostMessage(windowHandle, WM_CLOSE, 0, 0);
-		break;
-	}
+  switch (wParam) {
+    case VK_ESCAPE: {
+      PostMessage(windowHandle, WM_CLOSE, 0, 0);
+      break;
+    }
+  }
 }
